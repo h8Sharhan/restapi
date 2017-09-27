e19a6bf6c68f2756e1a8b9d734b7e8535235289e
oskotsyk <oleksii.skotsyk@gmail.com>
2017-09-27 10:32:38 -0700

    RD: add tests to cover fail of fetching data

--- app-test.py
+++ app-test.py
@@ -1,7 +1,9 @@
 import base64
-import json
 import unittest
+from json import loads
+from mock import patch
 
+import external_api_exceptions
 from app import app
 
 
@@ -16,56 +18,72 @@ class AppTestCase(unittest.TestCase):
         response = tester.get('/api/v1.0/random_word',
                               headers={'Authorization': 'Basic %s' % base64.b64encode(bytes("user:qwe123"))})
         self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data)
+        data = loads(response.data)
         self.assertEqual(data.get('status'), 'success')
         self.assertTrue(data.get('result'))
+
+        with patch('helper_functions.get_random_word') as get_random_word_mock:
+            get_random_word_mock.side_effect = external_api_exceptions.ExternalApiFetchError
+            response = tester.get('/api/v1.0/random_word',
+                                  headers={'Authorization': 'Basic %s' % base64.b64encode(bytes("user:qwe123"))})
+            self.assertEqual(response.status_code, 503)
 
     def test_wiki(self):
         tester = app.test_client(self)
 
         response = tester.get('/api/v1.0/wiki/Ninja')
         self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data)
+        data = loads(response.data)
         self.assertEqual(data.get('status'), 'success')
         self.assertTrue(data.get('result'))
 
         response = tester.get('/api/v1.0/wiki/Ninjasaur')
         self.assertEqual(response.status_code, 404)
 
+        with patch('helper_functions.get_wiki_article') as get_wiki_article_mock:
+            get_wiki_article_mock.side_effect = external_api_exceptions.ExternalApiFetchError
+            response = tester.get('/api/v1.0/wiki/Ninja')
+            self.assertEqual(response.status_code, 503)
+
     def test_most_popular(self):
         tester = app.test_client(self)
         response = tester.get('/api/v1.0/wiki/most_popular/3')
         self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data)
+        data = loads(response.data)
         self.assertEqual(data.get('status'), 'success')
         self.assertEqual(len(data.get('result')), 3)
 
     def test_joke(self):
         tester = app.test_client(self)
+
         response = tester.get('/api/v1.0/joke')
         self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data)
+        data = loads(response.data)
         self.assertEqual(data.get('status'), 'success')
         self.assertTrue(data.get('result'))
 
         response = tester.get('/api/v1.0/joke?firstName=Vasili')
         self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data)
+        data = loads(response.data)
         self.assertEqual(data.get('status'), 'success')
         self.assertTrue(data.get('result'))
 
         response = tester.get('/api/v1.0/joke?lastName=Dou')
         self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data)
+        data = loads(response.data)
         self.assertEqual(data.get('status'), 'success')
         self.assertTrue(data.get('result'))
 
         response = tester.get('/api/v1.0/joke?firstName=Vasili&lastName=Dou')
         self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data)
+        data = loads(response.data)
         self.assertEqual(data.get('status'), 'success')
         self.assertTrue(data.get('result'))
 
+        with patch('helper_functions.get_chuck_norris_joke') as get_chuck_norris_joke_mock:
+            get_chuck_norris_joke_mock.side_effect = external_api_exceptions.ExternalApiFetchError
+            response = tester.get('/api/v1.0/joke')
+            self.assertEqual(response.status_code, 503)
 
 if __name__ == '__main__':
     unittest.main()
